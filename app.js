dcae6f928f4d2faa81dbd4a7c4c23653f3c763b1
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2016-02-24 19:57:59 +0100

    Corregida la definición de la cadena de conexión para el entorno de Producción.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 var cadena_bbdd = "";
 
 if(process.env.MONGOLAB_URL){
-    cadena_bbdd = 'mongodb://[user]:[password]@[host]:[port]';
+    cadena_bbdd = process.env.MONGOLAB_URL;
 }else{
     cadena_bbdd = 'mongodb://localhost/jmlp';
 }
