987ead2ece59e9ad3521e5eed9d1065ec736f440
Juanma <juanmalp1992@gmail.com>
2016-03-03 15:57:19 +0100

    Añadidas todas las asignaturas del primer curso académico.

--- app/public/js/angular/controllers/expedienteAcademico.js
+++ app/public/js/angular/controllers/expedienteAcademico.js
@@ -2,12 +2,25 @@
  * Created by JuanManuel on 13/02/2016.
  */
 
-angular.module('jmlp').controller('ExpedienteAcademicoController', function($scope, $location, $window){
+angular.module('jmlp').controller('ExpedienteAcademicoController', function($scope, $location, $window, $filter){
 
     $scope.$parent.title = "Expediente Académico";
 
     $scope.notasExpediente = [
         {
+            curso: "1",
+            cursoAcademico: "2010/2011",
+            cuatrimestre: 1,
+            abreviatura: "FFI",
+            departamento: "FA1",
+            asignatura: "Fundamentos Físicos de la Informática",
+            nota: 5,
+            metodoAprobado: "Convocatoria",
+            convocatoriaUtilizada: true,
+            creditos: 6,
+            tipoCreditos: "Troncal/Formación básica",
+            numeroConvocatoria: 1
+        },{
             curso: "1",
             cursoAcademico: "2010/2011",
             cuatrimestre: 0,
@@ -59,8 +72,88 @@ angular.module('jmlp').controller('ExpedienteAcademicoController', function($sco
             creditos: 6,
             tipoCreditos: "Troncal/Formación básica",
             numeroConvocatoria: 1
+        },{
+            curso: "1",
+            cursoAcademico: "2010/2011",
+            cuatrimestre: 1,
+            abreviatura: "CIN",
+            departamento: "MA1",
+            asignatura: "Cálculo Infinitesimal y Numérico",
+            nota: 5.9,
+            metodoAprobado: "Parciales",
+            convocatoriaUtilizada: true,
+            creditos: 6,
+            tipoCreditos: "Troncal/Formación básica",
+            numeroConvocatoria: 1
+        },{
+            curso: "1",
+            cursoAcademico: "2010/2011",
+            cuatrimestre: 1,
+            abreviatura: "IMD",
+            departamento: "MA1",
+            asignatura: "Introducción a la Matemática Discreta",
+            nota: 6.5,
+            metodoAprobado: "Convocatoria",
+            convocatoriaUtilizada: true,
+            creditos: 6,
+            tipoCreditos: "Troncal/Formación básica",
+            numeroConvocatoria: 1
+        },{
+            curso: "1",
+            cursoAcademico: "2010/2011",
+            cuatrimestre: 2,
+            abreviatura: "EC",
+            departamento: "TE",
+            asignatura: "Estructura de Computadores",
+            nota: 6.3,
+            metodoAprobado: "Parciales",
+            convocatoriaUtilizada: true,
+            creditos: 6,
+            tipoCreditos: "Troncal/Formación básica",
+            numeroConvocatoria: 1
+        },{
+            curso: "1",
+            cursoAcademico: "2010/2011",
+            cuatrimestre: 2,
+            abreviatura: "E",
+            departamento: "EIO",
+            asignatura: "Estadística",
+            nota: 5.5,
+            metodoAprobado: "Parciales",
+            convocatoriaUtilizada: true,
+            creditos: 6,
+            tipoCreditos: "Troncal/Formación básica",
+            numeroConvocatoria: 1
+        },{
+            curso: "1",
+            cursoAcademico: "2010/2011",
+            cuatrimestre: 2,
+            abreviatura: "ALN",
+            departamento: "MA1",
+            asignatura: "Álgebra Lineal y Numérica",
+            nota: 5.4,
+            metodoAprobado: "Parciales",
+            convocatoriaUtilizada: true,
+            creditos: 6,
+            tipoCreditos: "Troncal/Formación básica",
+            numeroConvocatoria: 1
+        },{
+            curso: "1",
+            cursoAcademico: "2010/2011",
+            cuatrimestre: 2,
+            abreviatura: "AE",
+            departamento: "OIGE",
+            asignatura: "Administración de Empresas",
+            nota: 5.3,
+            metodoAprobado: "Parciales",
+            convocatoriaUtilizada: true,
+            creditos: 6,
+            tipoCreditos: "Troncal/Formación básica",
+            numeroConvocatoria: 1
         }
     ];
+
+    $scope.notasExpediente = $filter('orderBy')($scope.notasExpediente, 'abreviatura', false);
 
     $scope.abrirEnlace = function(enlace){
         $window.open(enlace, "_blank");
@@ -85,7 +178,7 @@ angular.module('jmlp').controller('ExpedienteAcademicoController', function($sco
         return res;
     };
 
-    $scope.getNotaMasReciente = function(asignatura){
+    $scope.getNotaMasRecienteDelCursoAcademico = function(cursoAcademico, asignatura){
 
         var res = 0;
 
@@ -93,10 +186,12 @@ angular.module('jmlp').controller('ExpedienteAcademicoController', function($sco
 
             var aux = $scope.notasExpediente[i];
 
-            if(aux.asignatura == asignatura){
+            if(aux.asignatura == asignatura && aux.cursoAcademico == cursoAcademico){
 
                 if(res == 0){
-                    res = aux.nota;
+                    if(!isNaN(aux.nota)){
+                        res = aux.nota;
+                    }
                 }else{
                     res = res > aux.nota ? res: aux.nota;
                 }
