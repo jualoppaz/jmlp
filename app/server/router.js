9404c00d4b12a3ad0f95fae345979fc9c9761429
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2018-11-02 21:50:25 +0100

    #43 Modificación de rutas del servidor para que se envíe el fichero "index.html" siempre que corresponda.

--- app/server/router.js
+++ app/server/router.js
@@ -1,12 +1,9 @@
 var express = require("express");
 var router = express.Router();
-var jwt = require('jwt-simple');
-var config = require('./config');
-
-
+var jwt = require("jwt-simple");
+var config = require("./config");
 
 function createToken(user) {
-
     var expire = new Date();
     expire.setSeconds(expire.getSeconds() + 5);
 
@@ -21,32 +18,31 @@ function createToken(user) {
     return jwt.encode(payload, config.tokenSecret);
 }
 
-require('./routes/temas/api.js')(router);
+require("./routes/temas/api.js")(router);
 
-router.post('/api/login', function(req, res){
-
+router.post("/api/login", function(req, res) {
     var user = req.body;
 
     console.log(req.body);
 
-    if(user.email == "juanmalp1992@gmail.com" && user.password == "12345"){
+    if (user.email == "juanmalp1992@gmail.com" && user.password == "12345") {
         var token = createToken(user);
         res.status(200).send({ token: token, user: user });
-    }else{
-        res.status(400).send('not-valid-login');
+    } else {
+        res.status(400).send("not-valid-login");
     }
 });
 
-router.get('/api/*', function(req, res){
-   res.status(404).send('not-found');
+router.get("/api/*", function(req, res) {
+    res.status(404).send("not-found");
 });
 
-router.get('/google1e2e247e7cbf40b6.html', function(req, res){
-    res.status(200).render('google1e2e247e7cbf40b6');
+router.get("/google1e2e247e7cbf40b6.html", function(req, res) {
+    res.status(200).render("google1e2e247e7cbf40b6");
 });
 
-router.get('/*', function(req, res){
-    res.render('index');
+router.get("/*", function(req, res) {
+    return res.sendFile(path.join(__dirname, "../../public", "index.html"));
 });
 
 module.exports = router;
