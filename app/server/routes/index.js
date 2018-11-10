b502a06621aa43b0071597e9728bf10e0fe86149
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2018-11-10 14:24:33 +0100

    #62 Implementación de ruta de API REST para el recurso GET /technologies/:technology_id y uso de middleware para validar campos de la request.

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -1,9 +1,23 @@
 module.exports = function(router) {
+    var ev = require("express-validation");
+    ev.options({
+        status: 422,
+        statusText: "Unprocessable Entity"
+    });
+
     var technologyController = require("../controllers/technologyController");
+    var validationsTechnology = require("../validations/technologyValidations");
 
     var express = require("express");
     var apiRouter = express.Router();
     apiRouter.route("/technologies").get(technologyController.index);
+
+    apiRouter
+        .route("/technologies/:technology_id")
+        .get(
+            ev(validationsTechnology.findTechnologyById),
+            technologyController.show
+        );
 
     router.use("/api", apiRouter);
 };
