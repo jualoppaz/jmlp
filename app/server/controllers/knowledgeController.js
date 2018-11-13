5ec8c31cec7c70d7335dfc992b8a2af213d27fc4
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2018-11-13 19:54:24 +0100

    #63 Implementación de consulta de conocimientos del CV, permitiendo el filtrado de los mismos por tipo de conocimiento.

---
+++ app/server/controllers/knowledgeController.js
@@ -0,0 +1,74 @@
+var Knowledge = require("../models/knowledge");
+var KnowledgeType = require("../models/knowledgeType");
+var Technology = require("../models/technology");
+
+var findAllKnowledge = function(req, res) {
+    var knowledgeTypeParams = {};
+
+    if (req.query.type) {
+        knowledgeTypeParams.name = req.query.type;
+    }
+
+    Knowledge.find(function(err, knowledge) {
+        if (err) {
+            return res.status(400).send(
+                JSON.stringify(
+                    {
+                        message: err
+                    },
+                    null,
+                    4
+                )
+            );
+        }
+
+        Technology.populate(knowledge, { path: "technology" }, function(
+            err,
+            knowledge
+        ) {
+            if (err) {
+                return res.status(400).send(
+                    JSON.stringify(
+                        {
+                            message: err
+                        },
+                        null,
+                        4
+                    )
+                );
+            }
+
+            KnowledgeType.populate(
+                knowledge,
+                {
+                    path: "type"
+                },
+                function(err, knowledge) {
+                    if (err) {
+                        return res.status(400).send(
+                            JSON.stringify(
+                                {
+                                    message: err
+                                },
+                                null,
+                                4
+                            )
+                        );
+                    }
+
+                    if (knowledgeTypeParams.name) {
+                        knowledge = knowledge.filter(function(item) {
+                            return item.type.name === knowledgeTypeParams.name;
+                        });
+                    }
+
+                    res.status(200).send(JSON.stringify(knowledge, null, 4));
+                }
+            );
+        });
+    });
+};
+
+module.exports = {
+    index: findAllKnowledge
+};
