a7ce7e3c3c545df5eaf9d0dbb8ccbf5e0bafc985
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2018-11-18 22:36:48 +0100

    #66 Implementación de consulta de proyectos personales en el "curriculumService".

--- app/angular/services/curriculum.service.js
+++ app/angular/services/curriculum.service.js
@@ -939,377 +939,6 @@
                     certificado: "certificado_hploadrunner.pdf"
                 }
             ],
-            proyectos: [
-                {
-                    resumen:
-                        "Desarrollo de la aplicación web CIDA: Continuous Integration Demo App",
-                    descripcion:
-                        "Desarrollo de una aplicación web realizada con AngularJS, la cual incluye una Integración " +
-                        "Continua muy completa con la herramienta Brunch, de modo que sea pueda agilizar el desarrollo " +
-                        "desde la compilación de assets hasta su despliegue optimizado en un sistema como Heroku.",
-                    technologies: [
-                        {
-                            image: "/images/tecnologia_angularjs.png",
-                            name: "AngularJS",
-                            web: "https://angularjs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_brunch.png",
-                            name: "Brunch",
-                            web: "https://brunch.io"
-                        },
-                        {
-                            image: "/images/tecnologia_sass.png",
-                            name: "Sass",
-                            web: "https://sass-lang.com"
-                        },
-                        {
-                            image: "/images/tecnologia_npm.png",
-                            name: "NPM",
-                            web: "https://www.npmjs.com"
-                        },
-                        {
-                            image: "/images/tecnologia_bower.png",
-                            name: "Bower",
-                            web: "https://bower.io"
-                        },
-                        {
-                            image: "/images/tecnologia_nodejs.png",
-                            name: "NodeJS",
-                            web: "https://nodejs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_jshint.png",
-                            name: "JSHint",
-                            web: "https://jshint.com"
-                        }
-                    ],
-                    desde: "02/2017",
-                    hasta: "Actualidad",
-                    imagen: "/images/tecnologia_brunch.png",
-                    enlaces: [
-                        {
-                            tipo: "web",
-                            nombre: "https://cida.herokuapp.com",
-                            ruta: "https://cida.herokuapp.com"
-                        },
-                        {
-                            tipo: "repositorio",
-                            nombre: "https://github.com/jualoppaz/cida",
-                            ruta: "https://github.com/jualoppaz/cida"
-                        }
-                    ]
-                },
-                {
-                    resumen: "Desarrollo de mi página web personal",
-                    descripcion:
-                        "Desarrollo de mi página web personal para darme a conocer, donde se puede consultar mi currículum y " +
-                        "mi expediente académico, entre otras cosas. Esta aplicación web no es igual que el resto, ya que ha sido " +
-                        "realizada con la filosofía SPA (Single Page Application). Esto quiere decir que consta de una " +
-                        "sola página con diferentes vistas, de modo que no se necesita recargar la página debido a que las vistas " +
-                        "se encuentran en el cliente y no en el servidor.",
-                    technologies: [
-                        {
-                            image: "/images/tecnologia_angularjs.png",
-                            name: "AngularJS",
-                            web: "https://angularjs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_nodejs.png",
-                            name: "Node.js",
-                            web: "https://nodejs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_pug.png",
-                            name: "Pug",
-                            web: "https://pugjs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_mongodb.png",
-                            name: "MongoDB",
-                            web: "https://www.mongodb.com"
-                        },
-                        {
-                            image: "/images/tecnologia_bootstrap.png",
-                            name: "Bootstrap",
-                            web: "https://getbootstrap.com"
-                        }
-                    ],
-                    desde: "03/2016",
-                    hasta: "Actualidad",
-                    imagen: "/images/yo_web4.jpg",
-                    bordeImagen: "img-circle",
-                    enlaces: [
-                        {
-                            tipo: "web",
-                            nombre: "http://www.juanmanuellopezpazos.es",
-                            ruta: "http://www.juanmanuellopezpazos.es"
-                        }
-                    ]
-                },
-                {
-                    resumen:
-                        "Desarrollo de la página web para la empresa Rayosol Toldos",
-                    descripcion:
-                        "Página web para la empresa Rayosol Toldos con el objetivo de mostrar los distintos modelos de toldos que fabrican e instalan," +
-                        "así como facilitar a los usuarios una forma de contacto inmediata mediante el envío de emails desde la propia página.",
-                    technologies: [
-                        {
-                            image: "/images/tecnologia_angularjs.png",
-                            name: "AngularJS",
-                            web: "https://angularjs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_nodejs.png",
-                            name: "Node.js",
-                            web: "https://nodejs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_pug.png",
-                            name: "Pug",
-                            web: "https://pugjs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_bootstrap.png",
-                            name: "Bootstrap",
-                            web: "https://getbootstrap.com"
-                        }
-                    ],
-                    desde: "02/2015",
-                    hasta: "06/2015",
-                    imagen: "/images/proyecto_rayosol.png",
-                    enlaces: [
-                        {
-                            tipo: "web",
-                            nombre: "http://www.rayosoltoldos.com",
-                            ruta: "http://www.rayosoltoldos.com"
-                        }
-                    ]
-                },
-                {
-                    resumen:
-                        "Desarrollo de la página web de Complementos Marta Pazos",
-                    descripcion:
-                        "Desarrollo de la página web de Complementos Marta Pazos la cual consta de: un amplio catálogo de productos," +
-                        " panel de administración de todo tipo de contenidos de la web como noticias o detalles de los productos, comercio " +
-                        "electrónico integrando la API REST de Paypal con el carrito de compras y un diseño totalmente personalizado acorde con el oficio.",
-                    technologies: [
-                        {
-                            image: "/images/tecnologia_angularjs.png",
-                            name: "AngularJS",
-                            web: "https://angularjs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_nodejs.png",
-                            name: "Node.js",
-                            web: "https://nodejs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_mongodb.png",
-                            name: "MongoDB",
-                            web: "https://www.mongodb.com"
-                        },
-                        {
-                            image: "/images/tecnologia_pug.png",
-                            name: "Pug",
-                            web: "https://pugjs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_bootstrap.png",
-                            name: "Bootstrap",
-                            web: "https://getbootstrap.com"
-                        },
-                        {
-                            image: "/images/tecnologia_fontAwesome.png",
-                            name: "Font Awesome",
-                            web: "https://fontawesome.com"
-                        }
-                    ],
-                    desde: "10/2014",
-                    hasta: "01/2015",
-                    imagen: "/images/proyecto_complementosMartaPazos.png",
-                    enlaces: [
-                        {
-                            tipo: "web",
-                            nombre: "http://www.complementosmartapazos.com",
-                            ruta: "http://www.complementosmartapazos.com"
-                        }
-                    ]
-                },
-                {
-                    resumen: "Desarrollo de la aplicación web Pronostigol",
-                    descripcion:
-                        "Aplicación web para consultar estadísticas relacionadas con los distintos sorteos de Loterías y Apuestas " +
-                        "del Estado (LAE).",
-                    technologies: [
-                        {
-                            image: "/images/tecnologia_angularjs.png",
-                            name: "AngularJS",
-                            web: "https://angularjs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_nodejs.png",
-                            name: "Node.js",
-                            web: "https://nodejs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_mongodb.png",
-                            name: "MongoDB",
-                            web: "https://www.mongodb.com"
-                        },
-                        {
-                            image: "/images/tecnologia_pug.png",
-                            name: "Pug",
-                            web: "https://pugjs.org"
-                        },
-                        {
-                            image: "/images/tecnologia_bootstrap.png",
-                            name: "Bootstrap",
-                            web: "https://getbootstrap.com"
-                        },
-                        {
-                            image: "/images/tecnologia_fontAwesome.png",
-                            name: "Font Awesome",
-                            web: "https://fontawesome.com"
-                        }
-                    ],
-                    desde: "05/2014",
-                    hasta: "Actualidad",
-                    imagen: "/images/proyecto_pronostigol.png",
-                    enlaces: [
-                        {
-                            tipo: "repositorio",
-                            nombre: "https://github.com/jualoppaz/pronostigol",
-                            ruta: "https://github.com/jualoppaz/pronostigol"
-                        },
-                        {
-                            tipo: "web",
-                            nombre: "http://www.pronostigol.es",
-                            ruta: "http://www.pronostigol.es"
-                        }
-                    ]
-                },
-                {
-                    resumen: "Desarrollo de la aplicación web EatHub",
-                    descripcion:
-                        "Aplicación web para crear, compartir y personalizar recetas. Proyecto realizado durante la asignatura " +
-                        "ISPP (Ingeniería del Software y Práctica Profesional) del cuarto curso del Grado en Ingniería Informática - Ingeniería " +
-                        "del Software de la Universidad de Sevilla.",
-                    desde: "02/2014",
-                    hasta: "09/2014",
-                    imagen: "/images/proyecto_eathub.png",
-                    technologies: [
-                        {
-                            name: "Django",
-                            image: "/images/tecnologia_django.png",
-                            web: "https://www.djangoproject.com"
-                        },
-                        {
-                            name: "MongoDB",
-                            image: "/images/tecnologia_mongodb.png",
-                            web: "https://www.mongodb.com"
-                        },
-                        {
-                            name: "HTML5",
-                            image: "/images/tecnologia_html5.png"
-                        },
-                        {
-                            name: "Bootstrap",
-                            image: "/images/tecnologia_bootstrap.png",
-                            web: "https://getbootstrap.com"
-                        }
-                    ],
-                    enlaces: [
-                        {
-                            tipo: "web",
-                            nombre: "http://eat-hub.herokuapp.com",
-                            ruta: "http://eat-hub.herokuapp.com"
-                        },
-                        {
-                            tipo: "repositorio",
-                            nombre: "https://github.com/EatHub",
-                            ruta: "https://github.com/EatHub"
-                        }
-                    ]
-                },
-                {
-                    resumen:
-                        "Trabajo de Fin de Grado: Desarrollo de la página web de Caramelos La Gloria",
-                    descripcion:
-                        "Página web para la empresa Caramelos La Gloria con el objetivo de mostrar un amplio catálogo de " +
-                        "productos, todo ello con un diseño innovador y atractivo para los usuarios. Este proyecto fue presentado como Trabajo " +
-                        "de Fin de Grado en el Grado en Ingeniería Informática - Ingeniería del Software.",
-                    desde: "12/2013",
-                    hasta: "09/2014",
-                    imagen: "/images/proyecto_lagloria.png",
-                    technologies: [
-                        {
-                            name: "MongoDB",
-                            image: "/images/tecnologia_mongodb.png",
-                            web: "https://www.mongodb.com"
-                        },
-                        {
-                            name: "Node.js",
-                            image: "/images/tecnologia_nodejs.png",
-                            web: "https://nodejs.org"
-                        },
-                        {
-                            name: "AngularJS",
-                            image: "/images/tecnologia_angularjs.png",
-                            web: "https://angularjs.org"
-                        },
-                        {
-                            name: "Pug",
-                            image: "/images/tecnologia_pug.png",
-                            web: "https://pugjs.org"
-                        },
-                        {
-                            name: "Bootstrap",
-                            image: "/images/tecnologia_bootstrap.png",
-                            web: "https://getbootstrap.com"
-                        },
-                        {
-                            name: "Font Awesome",
-                            image: "/images/tecnologia_fontAwesome.png",
-                            web: "https://fontawesome.com"
-                        }
-                    ],
-                    enlaces: [
-                        {
-                            tipo: "web",
-                            nombre: "http://carameloslagloria.herokuapp.com",
-                            ruta: "http://carameloslagloria.herokuapp.com"
-                        },
-                        {
-                            tipo: "repositorio",
-                            nombre: "https://github.com/jualoppaz/lagloria",
-                            ruta: "https://github.com/jualoppaz/lagloria"
-                        },
-                        {
-                            tipo: "Memoria",
-                            nombre: "Documentación del proyecto",
-                            ruta: "/docs/memoria_tfg.pdf"
-                        }
-                    ]
-                },
-                {
-                    resumen:
-                        "Usuario activo en la comunidad de Desarolladores GitHub",
-                    descripcion:
-                        "En mi perfil de GitHub tengo publicados multitud de proyectos para tener copias de seguridad de los mismos y, " +
-                        "además, para ayudar a otros usuarios con soluciones propuestas con distintas tecnologías.",
-                    desde: "12/2013",
-                    hasta: "Actualidad",
-                    imagen: "/images/tecnologia_github.png",
-                    enlaces: [
-                        {
-                            tipo: "repositorio",
-                            nombre: "https://github.com/jualoppaz",
-                            ruta: "https://github.com/jualoppaz"
-                        }
-                    ]
-                }
-            ],
             idiomas: [
                 {
                     nombre: "Inglés",
@@ -1324,7 +953,8 @@
 
         return {
             getCV: getCVFn,
-            getCVKnowledge: getCVKnowledgeFn
+            getCVKnowledge: getCVKnowledgeFn,
+            getCVPersonalProjects: getCVPersonalProjectsFn
         };
 
         ////////////////////
@@ -1346,6 +976,27 @@
                 .get(API.BASE_URL + API.CV + API.KNOWLEDGE, {
                     params: queryParameters
                 })
+                .then(function(data) {
+                    defered.resolve(data.data);
+                })
+                .catch(function(err) {
+                    defered.reject(err.data);
+                });
+
+            return promise;
+        }
+
+        /**
+         * Método que sirve para consultar el listado de proyectos personales del CV.
+         *
+         * @author jualoppaz
+         */
+        function getCVPersonalProjectsFn() {
+            var defered = $q.defer();
+            var promise = defered.promise;
+
+            $http
+                .get(API.BASE_URL + API.CV + API.PERSONAL_PROJECTS)
                 .then(function(data) {
                     defered.resolve(data.data);
                 })
