7b6c2bcacaf0d5e045d2176754b05e3deb6c759d
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2018-12-02 14:17:02 +0100

    #70 Implementación de consulta de Experiences Profesionales de mi CV.

---
+++ app/server/controllers/professionalExperienceController.js
@@ -0,0 +1,68 @@
+var ProfessionalExperience = require("../models/professionalExperience");
+
+var findAllProfessionalExperiences = function(req, res) {
+    ProfessionalExperience.find()
+        .populate({
+            path: "company"
+        })
+        .populate({
+            path: "projects.technologies"
+        })
+        .populate({
+            path: "clients.company"
+        })
+        .populate({
+            path: "clients.projects.technologies"
+        })
+        .exec(function(err, professionalExperiences) {
+            if (err) {
+                return res.status(400).send(
+                    JSON.stringify(
+                        {
+                            message: err
+                        },
+                        null,
+                        4
+                    )
+                );
+            }
+
+            professionalExperiences.forEach(function(professionalExperience) {
+                if (
+                    professionalExperience.projects &&
+                    professionalExperience.projects.length
+                ) {
+                    professionalExperience.projects.forEach(function(project) {
+                        project.technologies.sort(function(a, b) {
+                            return a.name.localeCompare(b.name);
+                        });
+                    });
+                }
+
+                if (
+                    professionalExperience.clients &&
+                    professionalExperience.clients.length
+                ) {
+                    professionalExperience.clients.forEach(function(client) {
+                        client.projects.forEach(function(project) {
+                            project.technologies.sort(function(a, b) {
+                                return a.name.localeCompare(b.name);
+                            });
+                        });
+                    });
+                }
+            });
+
+            professionalExperiences.sort(function(a, b) {
+                return b.startDate - a.startDate;
+            });
+
+            return res
+                .status(200)
+                .send(JSON.stringify(professionalExperiences, null, 4));
+        });
+};
+
+module.exports = {
+    index: findAllProfessionalExperiences
+};
