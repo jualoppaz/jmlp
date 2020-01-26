6be07aac290bbd0acaf54e627abb781960785d59
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-01-26 22:58:40 +0100

    #51 Implementación de método para obtener dinámicamente el nombre del label a mostrar dada la nota de una asignatura

--- app/angular/views/curriculum/academic-record/academicRecord.js
+++ app/angular/views/curriculum/academic-record/academicRecord.js
@@ -163,5 +163,14 @@
                 );
             }).length;
         };
+
+        $scope.getLabelClassByMark = (mark) => {
+            if((mark.nota >=5 && mark.nota <7) || mark.nota === 'APTO') return 'success';
+            if(mark.nota <5) return 'fail';
+            if (mark.nota >=7 && mark.nota <9) return 'primary';
+            if(mark.nota >=9 && !mark.matriculaHonor) return 'warning';
+            if (mark.matriculaHonor) return 'danger';
+
+        };
     }
 })();
