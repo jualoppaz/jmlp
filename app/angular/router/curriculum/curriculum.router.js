dc8b74d1aec7fb232ba9b2f11a487ff5a0a8e454
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-02-01 15:15:38 +0100

    #51 Implementación de resolves en la ruta de expediente académico con los datos necesarios para mostrar en pantalla

--- app/angular/router/curriculum/curriculum.router.js
+++ app/angular/router/curriculum/curriculum.router.js
@@ -103,6 +103,92 @@
                         templateUrl:
                             "app/angular/views/curriculum/academic-record/academicRecord.html"
                     }
+                },
+                resolve: {
+                    academicRecordResolve: function(academicRecordService){
+                        return {
+                            academicCourses: [
+                                {
+                                    course: "2010/2011",
+                                    data: {
+                                        firstQuarter: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2010/2011", 1),
+                                        secondQuarter: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2010/2011", 2),
+                                        allYear: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2010/2011", null)
+                                    }
+                                },{
+                                    course: "2011/2012",
+                                    data: {
+                                        firstQuarter: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2011/2012", 1),
+                                        secondQuarter: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2011/2012", 2),
+                                        allYear: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2011/2012", null)
+                                    }
+                                }, {
+                                    course: "2012/2013",
+                                    data: {
+                                        firstQuarter: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2012/2013", 1),
+                                        secondQuarter: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2012/2013", 2),
+                                        allYear: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2012/2013", null)
+                                    }
+                                },{
+                                    course: "2013/2014",
+                                    data: {
+                                        firstQuarter: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2013/2014", 1),
+                                        secondQuarter: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2013/2014", 2),
+                                        allYear: academicRecordService.getSubjectMarksByAcademicCourseAndQuarter("2013/2014", null)
+                                    }
+                                },
+                            ],
+                            "degreeCourses": [
+                                {
+                                    course: "1",
+                                    data: {
+                                        firstQuarter: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("1", 1),
+                                        secondQuarter: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("1", 2),
+                                        allYear: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("1", null)
+                                    }
+                                },{
+                                    course: "2",
+                                    data: {
+                                        firstQuarter: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("2", 1),
+                                        secondQuarter: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("2", 2),
+                                        allYear: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("2", null)
+                                    }
+                                }, {
+                                    course: "3",
+                                    data: {
+                                        firstQuarter: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("3", 1),
+                                        secondQuarter: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("3", 2),
+                                        allYear: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("3", null)
+                                    }
+                                }, {
+                                    course: "4",
+                                    data: {
+                                        firstQuarter: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("4", 1),
+                                        secondQuarter: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("4", 2),
+                                        allYear: academicRecordService.getSubjectMarksByDegreeCourseAndQuarter("4", null)
+                                    }
+                                },
+                            ]
+                        };
+                    },
+                    averageByAcademicCourseAndSubjectResolve: function(academicRecordService){
+                        return academicRecordService.getAverageByAcademicCourseAndSubject();
+                    },
+                    averageByAcademicCourseAndCreditResolve: function(academicRecordService){
+                        return academicRecordService.getAverageByAcademicCourseAndCredit();
+                    },
+                    averageByDegreeCourseAndSubjectResolve: function(academicRecordService){
+                        return academicRecordService.getAverageByDegreeCourseAndSubject();
+                    },
+                    averageByDegreeCourseAndCreditResolve: function(academicRecordService){
+                        return academicRecordService.getAverageByDegreeCourseAndCredit();
+                    },
+                    degreeAverageBySubjectResolve: function(academicRecordService){
+                        return academicRecordService.getDegreeAverageBySubject();
+                    },
+                    degreeAverageByCreditResolve: function(academicRecordService){
+                        return academicRecordService.getDegreeAverageByCredit();
+                    }
                 }
             });
     }
